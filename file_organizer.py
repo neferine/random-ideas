ec89cf22721b8fe3daed7d94e3b40b2229b0bd3f
neferine <jhosween@gmail.com>
2025-06-26 18:35:31 +0800

    Update file_organizer.py

--- file_organizer.py
+++ file_organizer.py
@@ -1,8 +1,36 @@
 import os
 import shutil
 
-#Set the folder path to the files you want to organize
-folder_path = '/home/pug/Downloads' 
+folder_path = '/home/pug/Downloads' #path to folder that you will organize
+
+#Home Directories
+home_dir = os.path.expanduser("~")
+pic_dir = os.path.join(home_dir,"Pictures")
+music_dir = os.path.join(home_dir,"Music")
+videos_dir = os.path.join(home_dir,"Videos")
+document_dir = os.path.join(home_dir,"Documents")
+
+#file formats
+image_formats = ['png', 'jpg', 'jpeg', 'gif', 'tiff', 'bmp']
+video_formats = ['mp4', 'mov', 'avi' , 'wmv', 'mkv' , 'webm']
+music_formats = ['mp3', 'aac' , 'wav', 'flac', 'aiff']
+document_formats = [
+    'pdf', 'doc', 'docx', 'txt', 'rtf',
+    'odt', 'md', 'tex', 'csv', 'xlsx', 'xls',
+    'ppt', 'pptx', 'json', 'xml', 'yaml', 'yml'
+]
+
+def check_folders(home_dir):
+    folders = ["Pictures", "Downloads", "Documents", "Music", "Videos"]
+
+    for folder in folders:
+        path = os.path.join(home_dir,folder)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+            print(f"Created missing {path} folder for ya!")
+    print("Folders are fine")
+
+check_folders(home_dir)
 
 for file in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file)
@@ -10,14 +38,17 @@ for file in os.listdir(folder_path):
     if os.path.isfile(file_path):
         ext = os.path.splitext(file)[1][1:].lower()
 
-        if ext:
-            dest_folder = os.path.join(folder_path, ext.upper())
-            os.makedirs(dest_folder, exist_ok=True)
-            
-            shutil.move(file_path, os.path.join(dest_folder, file))
+    try:
+        if ext in image_formats:
+            shutil.move(file_path , os.path.join(pic_dir, file))
+        elif ext in video_formats:
+            shutil.move(file_path, os.path.join(videos_dir, file))
+        elif ext in music_formats:
+            shutil.move(file_path, os.path.join(music_dir, file))
+        elif ext in document_formats:            
+            shutil.move(file_path, os.path.join(document_dir, file))
+
+    except Exception:
+        print("Error moving {file}")
 
 print("File Organization Complete!")
-
-
-
-
